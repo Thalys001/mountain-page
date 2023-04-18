46341792f5caae8bcbb748f66df2a3d5a17f72a1
Thalys001 <thalyssouza708@gmail.com>
2023-04-17 23:44:47 -0300

    refactor: adding code readability improvements

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-const mountainLeft = document.querySelector('#mountain_left');
-const mountainRight = document.querySelector('#mountain_right');
-const cloud1 = document.querySelector('#clouds_1');
-const cloud2 = document.querySelector('#clouds_2');
-const text = document.querySelector('#text');
-const man = document.querySelector('#man');
+const mountainLeftEl = document.querySelector("#mountain_left");
+const mountainRightEl = document.querySelector("#mountain_right");
+const cloud1El = document.querySelector("#clouds_1");
+const cloud2El = document.querySelector("#clouds_2");
+const textEl = document.querySelector("#text");
+const manEl = document.querySelector("#man");
 
+// Atualizar posição dos elementos HTML com base no valor de rolagem
+function updateElementsPosition() {
+  const scrollPosition = window.scrollY;
 
-window.addEventListener('scroll', ()=>{
-  let value = scrollY;
-  mountainLeft.style.left = `-${value/0.7}px`
-  cloud2.style.left = `-${value*2}px`
-  mountainRight.style.left = `${value/0.7}px`
-  cloud1.style.left = `${value*2}px`
-  text.style.bottom = `-${value}px`;
-  man.style.height = `${window.innerHeight - value}px`
-})
+  // Atualizar posição da montanha esquerda
+  mountainLeftEl.style.left = `-${scrollPosition / 0.7}px`;
+
+  // Atualizar posição da nuvem direita
+  cloud2El.style.left = `-${scrollPosition * 2}px`;
+
+  // Atualizar posição da montanha direita
+  mountainRightEl.style.left = `${scrollPosition / 0.7}px`;
+
+  // Atualizar posição da nuvem esquerda
+  cloud1El.style.left = `${scrollPosition * 2}px`;
+
+  // Atualizar posição do texto
+  textEl.style.bottom = `-${scrollPosition}px`;
+
+  // Atualizar altura da imagem do homem
+  manEl.style.height = `${window.innerHeight - scrollPosition}px`;
+}
+
+// Adicionar ouvinte de eventos para atualizar posição dos elementos
+window.addEventListener("scroll", updateElementsPosition);
